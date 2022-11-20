96a4574dc00b3d1329c63184f36634943279db10
BrandonF55 <brandon.fenk99@gmail.com>
2022-11-19 19:39:17 -0800

    tried making a new templete for my project. running into 'error:03000086:digital envelope routines::initialization error' every time i try npm run build and npm run start.

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import CurrencyService from './js/currency-api.js';
+
+
+
+// Business Logic_______________
+
+function getCurrency(currency, usd){
+  console.log(getCurrency);
+  let promise = CurrencyService.getCurrency(currency, usd);
+  promise.then(function(CurrencyDataArray) {
+    printElements(CurrencyDataArray);
+  }, function(error) {
+    printError(error);
+    console.log(getCurrency);
+  });
+
+  }
+
+  
+  // UI Logic_______________
+
+
+function printElements(response, currency, usd){
+  let exchangeAmount = response.conversion_rates;
+  document.querySelector("#output").innerHTML = `The exchange in ${usd} to ${currency} is worth ${exchangeAmount} `
+} 
+
+
+function printError(error){
+  let output = document.getElementById("output")
+  output.innerHTML = null;
+  if ( error.toString().includes('404')) {
+    output.innerHTML = printError()
+  } else {
+    output.innerHTML = printError();
+  }
+
+
+function handleSubit(event) {
+  event.preventDefault();
+  output.innerHTML =null;
+  const currency = document.getElementById("currency-type").value;
+  const usd = parseInt.getElementById("usd-input");
+  
+  getCurrency(currency, usd);
+}
+
+window.addEventListener("load", function() {
+this.document.querySelector("form").addEventListener("submit", handleSubit);
+
+});
+}
